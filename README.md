7e14fb9338da50e0f0eb6927ff0c631a5965a1ff
Leon-A1 <leonaviev@gmail.com>
2021-12-27 18:04:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Coinbase API based React web socket ticker. Tracking live crypto currency price through a web socket connection, retrieving chart data with fetch.
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+export const formatData = (data) => {
+  let finalData = {
+    labels: [],
+    datasets: [
+      {
+        label: "Price",
+        data: [],
+        backgroundColor: "rgb(67, 123, 191, 0.9)",
+        borderColor: "rgba(67, 123, 191, 0.4)",
+        fill: false,
+      },
+    ],
+  };
+
+  let dates = data.map((val) => {
+    const ts = val[0];
+    let date = new Date(ts * 1000);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let final = `${month}-${day}-${year}`;
+    return final;
+  });
+
+  let priceArr = data.map((val) => {
+    return val[4];
+  });
+
+  priceArr.reverse();
+  dates.reverse();
+  finalData.labels = dates;
+  finalData.datasets[0].data = priceArr;
+
+  return finalData;
+};
